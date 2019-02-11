b0d02261d4e78f612304793b81792332f003a587
Rez W <rezelute@gmail.com>
2019-02-11 17:59:24 +0000

    removed orig

--- src/api/server/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default {
-  fetchPosts () {
-    return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.data)
-  }
-}
